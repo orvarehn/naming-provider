acb018aecebcf941fc4f0f32e01a88e7a37093fe
Orvar <orvar.ehn@jayway.com>
2016-11-15 11:02:41 +0100

    First commit

---
+++ src/main/java/com/ikea/namingservice/NamingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ikea.namingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamingServiceApplication.class, args);
+	}
+}
